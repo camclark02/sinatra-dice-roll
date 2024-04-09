0ec65626e2e91263d418ff5cd7f554e9419b0b84
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-09 16:49:43 +0000

    added /dice/2/10

--- dice.rb
+++ dice.rb
@@ -31,6 +31,7 @@ end
 #get("/check") do
  # "This works!"
 #end
+
 get("/dice/2/6") do
   first_die = rand(1..6)
 	second_die = rand(1..6)
@@ -44,33 +45,11 @@ end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-	second_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
-	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
-end
 
-get("/dice/1/20") do
-  die = rand(1..20)
-	
-	outcome = "You rolled a #{die}."
-	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
-end
+  outcome = "You rolled a #{first_ die} and a #{second_die} for a total of #{sum}"
 
-get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
 end
